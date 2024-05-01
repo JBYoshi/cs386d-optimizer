9c31fe2e635d50c478845f2089fc9c7fed6c26b0
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2024-04-30 20:56:45 -0500

    Implement join selectivity function.

--- src/main/java/dev/jonathanb/cs386d/ColumnStats.java
+++ src/main/java/dev/jonathanb/cs386d/ColumnStats.java
@@ -6,40 +6,46 @@ import java.util.Map;
 // Nulls excluded in other fields
 // histogram excludes mostCommon
 public record ColumnStats(double fractionNull, long nDistinct, Map<HistogramValue, Double> mostCommon/*, List<HistogramRange> histogram*/) {
+    public double fractionUnmapped() {
+        return 1 - fractionNull - mostCommon.values().stream().mapToDouble(Double::doubleValue).sum();
+    }
+
+    public long nDistinctUnmapped() {
+        return nDistinct - mostCommon.size();
+    }
+
     public double semijoinSelectivityAgainst(ColumnStats join) {
-        double totalFrequencyOfOverlap = 0;
-        double unmappedFraction = 1 - fractionNull;
-        long unmappedNDistinctMine = nDistinct, unmappedNDistinctTheirs = join.nDistinct;
+        double sharedFractionMine = 0;
+        double unsharedFractionMine = 1 - fractionNull;
+        long unsharedDistinctMine = nDistinct, unsharedDistinctTheirs = join.nDistinct;
 
         // If a histogram entry from here is in the other relation, then that section is automatically kept.
         for (Map.Entry<HistogramValue, Double> myEntry : mostCommon.entrySet()) {
             if (join.mostCommon.containsKey(myEntry.getKey())) {
-                totalFrequencyOfOverlap += myEntry.getValue();
-                unmappedFraction -= myEntry.getValue();
-                unmappedNDistinctMine--;
-                unmappedNDistinctTheirs--;
+                sharedFractionMine += myEntry.getValue();
+                unsharedFractionMine -= myEntry.getValue();
+                unsharedDistinctMine--;
+                unsharedDistinctTheirs--;
             }
         }
 
         // The rest (if there is any) follows the original model.
         double semijoinSelectivityLeftoverPart = 0;
-        if (unmappedNDistinctMine > 0 && mostCommon.size() < nDistinct) {
-            semijoinSelectivityLeftoverPart = unmappedFraction * Math.min((double) unmappedNDistinctTheirs / unmappedNDistinctMine, 1);
+        if (unsharedDistinctMine > 0 && nDistinctUnmapped() > 0) { // TODO: one of these is redundant but I'm missing the brainpower to figure out which.
+            semijoinSelectivityLeftoverPart = unsharedFractionMine * Math.min((double) unsharedDistinctTheirs / unsharedDistinctMine, 1);
         }
 
-        return totalFrequencyOfOverlap + semijoinSelectivityLeftoverPart;
+        return sharedFractionMine + semijoinSelectivityLeftoverPart;
     }
 
     public ColumnStats semijoinStatsAgainst(ColumnStats join) {
-        double discardedFraction = fractionNull;
+        double unsharedFractionMine = 1 - fractionNull;
         Map<HistogramValue, Double> newMostCommon = new HashMap<>();
-        long discardedDistinct = 0;
         for (Map.Entry<HistogramValue, Double> myEntry : mostCommon.entrySet()) {
             if (join.mostCommon.containsKey(myEntry.getKey())) {
                 newMostCommon.put(myEntry.getKey(), myEntry.getValue());
             } else {
-                discardedFraction += myEntry.getValue();
-                discardedDistinct++;
+                unsharedFractionMine -= myEntry.getValue();
             }
         }
 
@@ -50,8 +56,68 @@ public record ColumnStats(double fractionNull, long nDistinct, Map<HistogramValu
             // New total = old total * (1 - discarded fraction)
             // New frequency * old total * (1 - discarded fraction) = old frequency * old total
             // New frequency * (1 - discarded fraction) = old frequency
-            entry.setValue(entry.getValue() / (1 - discardedFraction));
+            entry.setValue(entry.getValue() / unsharedFractionMine);
         }
-        return new ColumnStats(0, nDistinct - discardedDistinct, newMostCommon);
+
+        long newNDistinct;
+        if (nDistinct == mostCommon.size()) {
+            newNDistinct = newMostCommon.size();
+        } else {
+            newNDistinct = Math.min(nDistinct, join.nDistinct);
+        }
+        return new ColumnStats(0, newNDistinct, newMostCommon);
+    }
+
+    public double joinSelectivityAgainst(ColumnStats other) {
+        double sharedFractionMine = 0, sharedFractionTheirs = 0;
+        double unsharedFractionMine = 1 - fractionNull, unsharedFractionTheirs = 1 - fractionNull;
+        double fractionUnmappedMySide = 1 - fractionNull, fractionUnmappedTheirSide = 1 - fractionNull;
+        long unsharedDistinctMine = nDistinct, unsharedDistinctTheirs = other.nDistinct;
+        long nShared = 0;
+        double selectivity = 0;
+
+        // First consider values that exist in both histograms.
+        for (Map.Entry<HistogramValue, Double> myEntry : mostCommon.entrySet()) {
+            if (other.mostCommon.containsKey(myEntry.getKey())) {
+                sharedFractionMine += myEntry.getValue();
+                sharedFractionTheirs += other.mostCommon.get(myEntry.getKey());
+                unsharedFractionMine -= myEntry.getValue();
+                unsharedFractionTheirs -= other.mostCommon.get(myEntry.getKey());
+                selectivity += myEntry.getValue() * other.mostCommon.get(myEntry.getKey());
+                unsharedDistinctMine--;
+                unsharedDistinctTheirs--;
+                nShared++;
+            }
+        }
+
+        // Next consider values that exist in exactly one histogram.
+        // For values in one histogram, contributes (value * other.fractionUnmapped / other.nDistinctUnmapped) * (probability that this element is in the join).
+        // Semijoin sets probability to min((double) unsharedDistinctTheirs / unsharedDistinctMine, 1).
+        if (other.nDistinctUnmapped() > 0) {
+            for (Map.Entry<HistogramValue, Double> myEntry : mostCommon.entrySet()) {
+                if (!other.mostCommon.containsKey(myEntry.getKey())) {
+                    double probabilityThatThisExists = Math.min((double) unsharedDistinctTheirs / unsharedDistinctMine, 1);
+                    selectivity += myEntry.getValue() * other.fractionUnmapped() / other.nDistinctUnmapped() * probabilityThatThisExists;
+                }
+            }
+        }
+        if (nDistinctUnmapped() > 0) {
+            for (Map.Entry<HistogramValue, Double> theirEntry : other.mostCommon.entrySet()) {
+                if (!mostCommon.containsKey(theirEntry.getKey())) {
+                    double probabilityThatThisExists = Math.min((double) unsharedDistinctMine / unsharedDistinctTheirs, 1);
+                    selectivity += theirEntry.getValue() * fractionUnmapped() / nDistinctUnmapped() * probabilityThatThisExists;
+                }
+            }
+        }
+
+        // Finally consider values that are not in either histogram.
+        long numExplicitValues = mostCommon.size() + other.mostCommon.size() - nShared;
+        long numImplicitValuesSelf = nDistinct - numExplicitValues;
+        long numImplicitValuesOther = other.nDistinct - numExplicitValues;
+        if (numImplicitValuesSelf > 0 && numImplicitValuesOther > 0) {
+            selectivity += fractionUnmapped() * other.fractionUnmapped() / Math.max(numImplicitValuesSelf, numImplicitValuesOther);
+        }
+
+        return selectivity;
     }
 }
--- src/test/java/dev/jonathanb/cs386d/ColumnStatsTest.java
+++ src/test/java/dev/jonathanb/cs386d/ColumnStatsTest.java
@@ -8,25 +8,32 @@ import static org.junit.Assert.assertEquals;
 
 public class ColumnStatsTest {
     @Test
-    public void testSemijoinSelectivityNoFrequent() {
+    public void testJoinNoFrequent() {
         ColumnStats left = new ColumnStats(0, 5, Map.of());
         ColumnStats right = new ColumnStats(0, 10, Map.of());
         assertEquals(1, left.semijoinSelectivityAgainst(right), 0.001);
         assertEquals(0.5, right.semijoinSelectivityAgainst(left), 0.001);
+        // Say there is one entry of each value. Left has 1-5, right has 1-10.
+        // Cross product gives 50 tuples, of which 5 match. So it should be 0.1.
+        assertEquals(0.1, left.joinSelectivityAgainst(right), 0.001);
+        assertEquals(0.1, right.joinSelectivityAgainst(left), 0.001);
     }
 
     @Test
-    public void testSemijoinSelectivityBothFrequentOnly() {
+    public void testBothFrequentOnly() {
         ColumnStats left = new ColumnStats(0, 3,
                 Map.of(new HistogramValue("A"), 0.5, new HistogramValue("B"), 0.2, new HistogramValue("C"), 0.3));
         ColumnStats right = new ColumnStats(0, 3,
                 Map.of(new HistogramValue("B"), 0.25, new HistogramValue("C"), 0.35, new HistogramValue("D"), 0.4));
         assertEquals(0.5, left.semijoinSelectivityAgainst(right), 0.001);
         assertEquals(0.6, right.semijoinSelectivityAgainst(left), 0.001);
+
+        assertEquals(0.2 * 0.25 + 0.3 * 0.35, left.joinSelectivityAgainst(right), 0.001);
+        assertEquals(0.2 * 0.25 + 0.3 * 0.35, right.joinSelectivityAgainst(left), 0.001);
     }
 
     @Test
-    public void testSemijoinSelectivityEqualMix() {
+    public void testJoinEqualMix() {
         ColumnStats left = new ColumnStats(0, 4,
                 Map.of(new HistogramValue("A"), 0.4, new HistogramValue("B"), 0.5));
         ColumnStats right = new ColumnStats(0, 4,
@@ -34,10 +41,17 @@ public class ColumnStatsTest {
         // Both are 1 because each side has 4 elements.
         assertEquals(1, left.semijoinSelectivityAgainst(right), 0.001);
         assertEquals(1, right.semijoinSelectivityAgainst(left), 0.001);
+
+        // A gets 0.4 * 0.45 / 2
+        // B gets 0.5 * 0.25
+        // C gets 0.3 * 0.1 / 2
+        // D gets 0.45 / 2 * 0.1 / 2
+        assertEquals(0.4 * 0.45 / 2 + 0.5 * 0.25 + 0.3 * 0.1 / 2 + 0.45 / 2 * 0.1 * 2, left.joinSelectivityAgainst(right), 0.001);
+        assertEquals(0.4 * 0.45 / 2 + 0.5 * 0.25 + 0.3 * 0.1 / 2 + 0.45 / 2 * 0.1 * 2, right.joinSelectivityAgainst(left), 0.001);
     }
 
     @Test
-    public void testSemijoinSelectivityUnEqualMix() {
+    public void testJoinUnEqualMix() {
         ColumnStats left = new ColumnStats(0, 10,
                 Map.of(new HistogramValue("A"), 0.4, new HistogramValue("B"), 0.5));
         ColumnStats right = new ColumnStats(0, 4,
@@ -46,10 +60,24 @@ public class ColumnStatsTest {
         // Half from the match with B, so the other half is a 3 versus 9 join.
         assertEquals(0.5 + (3.0 / 9 / 2), left.semijoinSelectivityAgainst(right), 0.001);
         assertEquals(1, right.semijoinSelectivityAgainst(left), 0.001);
+
+        // A gets 0.4 * (0.45 / 2) * (3/9) = left.value * right.fractionUnmapped / right.nDistinctUnmapped * left.nDistinct / right.nDistinct
+        // B gets 0.5 * 0.25
+        // C gets (0.1 / 8) * 0.3 = right.value * left.fractionUnmapped / left.nDistinctUnmapped
+        // Rest gets (0.1 / 8) * (0.45 / 2) * 2 = 0.1 * 0.45 / 8 = left.fractionUnmapped * right.fractionUnmapped / max(left.distinctUnmapped, right.distinctUnmapped)
+        assertEquals(0.4 * (0.45 / 2) * (3.0 / 9)
+                + 0.5 * 0.25
+                + (0.1 / 8) * 0.3
+                + 0.1 * 0.45 / 8,
+                left.joinSelectivityAgainst(right), 0.001);
+        assertEquals(0.4 * (0.45 / 2) * (3.0 / 9)
+                + 0.5 * 0.25
+                + (0.1 / 8) * 0.3
+                + 0.1 * 0.45 / 8, right.joinSelectivityAgainst(left), 0.001);
     }
 
     @Test
-    public void testSemijoinSelectivityOneFrequentOnly() {
+    public void testJoinOneFrequentOnly() {
         ColumnStats left = new ColumnStats(0, 3,
                 Map.of(new HistogramValue("A"), 0.4, new HistogramValue("B"), 0.35, new HistogramValue("C"), 0.25));
         ColumnStats right = new ColumnStats(0, 10,
@@ -58,5 +86,20 @@ public class ColumnStatsTest {
         assertEquals(0.6, left.semijoinSelectivityAgainst(right), 0.001);
         // 0.35 from histogram exact match, 0.65 * 1/8 from rest
         assertEquals(0.35 + 0.65 / 8, right.semijoinSelectivityAgainst(left), 0.001);
+
+        // B is 0.35 * 0.25, C is 0.25 * 0.1, A is 0.4 * 0.55 (the leftover part)
+        assertEquals(0.35 * 0.25 + 0.25 * 0.1 + 0.4 * 0.55 / 7, left.joinSelectivityAgainst(right), 0.00001);
+    }
+
+    @Test
+    public void testJoin2x2() {
+        ColumnStats left = new ColumnStats(0, 2, Map.of(new HistogramValue("A"), 0.5));
+        ColumnStats right = new ColumnStats(0, 2, Map.of(new HistogramValue("B"), 0.5));
+        // Since we assume maximum overlap, this effectively means both are half A and half B.
+        // Full semijoin selectivity, and half join selectivity (keep AA and BB, not AB or BA).
+        assertEquals(1, left.semijoinSelectivityAgainst(right), 0.001);
+        assertEquals(1, right.semijoinSelectivityAgainst(left), 0.001);
+        assertEquals(0.5, left.joinSelectivityAgainst(right), 0.001);
+        assertEquals(0.5, right.joinSelectivityAgainst(left), 0.001);
     }
 }
