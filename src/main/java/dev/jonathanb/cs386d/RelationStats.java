857e182d3e467d9987a7775103643bff601bc3de
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2024-05-01 20:25:41 -0500

    Decrease nDistinct for fields with more distinct values than rows after a select.

--- src/main/java/dev/jonathanb/cs386d/RelationStats.java
+++ src/main/java/dev/jonathanb/cs386d/RelationStats.java
@@ -11,6 +11,14 @@ public record RelationStats(double numRows, Map<Column, ColumnStats> columnStats
         for (Map.Entry<Column, ColumnStats> entry : columnStats().entrySet()) {
             if (columns.contains(entry.getKey())) {
                 updatedColumns.put(entry.getKey(), selectivity.newStats());
+            } else if (entry.getValue().nDistinct() > count) {
+                // Common for ID fields; these are generally unique and so the number of unique values will also decrease proportionally.
+                ColumnStats decreasedStats = new ColumnStats(entry.getValue().fractionNull(),
+                        Math.round(entry.getValue().nDistinct() * selectivity.selectivity()),
+                        entry.getValue().mostCommon(),
+                        entry.getValue().histogram()
+                );
+                updatedColumns.put(entry.getKey(), decreasedStats);
             } else {
                 updatedColumns.put(entry.getKey(), entry.getValue());
             }
